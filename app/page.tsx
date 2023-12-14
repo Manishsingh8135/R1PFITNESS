d84bf86daea428e03aab3228214bc13e22e998c7
Manish <manish@Manishs-MacBook-Air.local>
2023-12-14 14:49:54 +0530

    some more commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,11 @@ import HeroSection from '@/components/Hero';
 import TextSection from '@/components/TextSection';
 import FaqSection from '@/components/FaqSection';
 import MainComponent from '@/components/featuresSection/MainComponent';
-import StaticPricingSection from '@/components/pricing/StaticPricingSection';
-import PriceSection from '@/components/pricing/StaticPricingSection';
 import NewPriceSection from '@/components/pricing/NewPriceSection';
 import TrainersPage from '@/components/Trainers/trainers';
-import TrainerProfilePage from '@/components/Trainers/OneTrainer';
+
 import AnimatedHeadingSection from '@/components/textSection/textContainer';
 import ImageGallery from '@/components/gallery/ImageGallery';
-import BMICalculator from '@/components/bmi/BMICalculator';
 
 // import CustomSwiper from '@/components/CoverflowSection';
 
@@ -30,8 +27,7 @@ const Home: React.FC = () => {
       <NewPriceSection />
       <TrainersPage />
       <ImageGallery/>
-      {/* <GComponent/> */}
-      <BMICalculator/>
+      
       <FaqSection />
 
     </div>
--- app/trainers/page.tsx
+++
@@ -1,7 +0,0 @@
-import TrainersPage from "@/components/Trainers/trainers";
-
-export default function Home(){
-    return(
-        <TrainersPage/>
-    )
-}
