7fe229878732328e834710dc4654d5f615215fe4
manish <manishsingh8135@gmail.com>
2024-02-07 13:19:37 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -34,6 +35,7 @@ export default function RootLayout({
             {/* <Navbar/> */}
             {children}
             <SpeedInsights />
+            <Analytics />
             <Footer/>
           </ThemeProvider>
         </body>
