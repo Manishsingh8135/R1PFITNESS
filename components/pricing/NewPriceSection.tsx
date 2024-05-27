da587608597f35a70fdeff886ed249c55372f8b1
manish <manishsingh8135@gmail.com>
2024-05-27 18:16:51 +0530

    added links to pricing

--- components/pricing/NewPriceSection.tsx
+++ components/pricing/NewPriceSection.tsx
@@ -118,7 +118,7 @@ export default function NewPriceSection() {
                                 </li>
                             </ul>
                             <a
-                                href="#"
+                                href="https://app.glofox.com/portal/#/branch/613baa9ab3d3686b1502dd03/memberships"
                                 className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
                             >
                                 Get started
@@ -228,7 +228,7 @@ export default function NewPriceSection() {
                                 </li>
                             </ul>
                             <a
-                                href="#"
+                                href="https://app.glofox.com/portal/#/branch/613baa9ab3d3686b1502dd03/memberships"
                                 className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
                             >
                                 Get started
@@ -335,7 +335,7 @@ export default function NewPriceSection() {
                                 </li>
                             </ul>
                             <a
-                                href="#"
+                                href="https://app.glofox.com/portal/#/branch/613baa9ab3d3686b1502dd03/memberships"
                                 className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
                             >
                                 Get started
