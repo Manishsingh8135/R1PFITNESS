efd5be7b3eed8b961901d1cd3551421245283fac
manish <manishsingh8135@gmail.com>
2024-06-13 14:19:09 +0530

    hover effect changes in table

--- components/table/MainTable.tsx
+++ components/table/MainTable.tsx
@@ -2,41 +2,43 @@
 "use client"
 import Image from "next/image";
 import { GymComparisonTable } from "./GymComparisonTable";
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react";
 
 export default function MainTable() {
-  const [isMobile, setIsMobile] = useState(false)
+  const [isMobile, setIsMobile] = useState(false);
 
   const checkScreenWidth = () => {
-    setIsMobile(window.innerWidth <= 768)
-  }
+    setIsMobile(window.innerWidth <= 768);
+  };
 
-     useEffect (() => {
-      checkScreenWidth();
-      window.addEventListener("resize", checkScreenWidth)
-      return () => {
-        window.removeEventListener("resize", checkScreenWidth)
-      }
-     }, [])
+  useEffect(() => {
+    checkScreenWidth();
+    window.addEventListener("resize", checkScreenWidth);
+    return () => {
+      window.removeEventListener("resize", checkScreenWidth);
+    };
+  }, []);
 
   return (
-    <div className="container mx-auto p-4 ">
-      <h1 className="text-4xl md:text-6xl font-bold  text-center my-12 text-maincolor">Choose Your Perfect Gym: A Detailed Comparison</h1>
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl md:text-6xl font-bold text-center my-12 text-maincolor">
+        Choose Your Perfect Gym: A Detailed Comparison
+      </h1>
       {isMobile ? (
-        <div className=" w-full flex items-center justify-center"> 
-          <Image
-          src="/assets/comparisontable/GymComparisonImg.png"
-          width = {500}
-          height= {500}
-          alt = "comparison"
-          />
+        <div className="w-full flex items-center justify-center">
+          <div className="relative group">
+            <Image
+              src="/assets/comparisontable/GymComparisonImg.png"
+              width={500}
+              height={500}
+              alt="comparison"
+              className="transition-transform duration-300 transform group-hover:scale-110"
+            />
+          </div>
         </div>
-        
-      ):(
+      ) : (
         <GymComparisonTable />
       )}
-      
-      
     </div>
   );
 }
